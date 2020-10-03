47f65063f0811080c305bda7bdcc0162edd1c5f7
Walter Derezinski <walter@daringway.com>
2020-10-03 00:28:31 -0400

    fixup! added fish withd

--- fish/functions/withd.fish
+++ fish/functions/withd.fish
@@ -2,9 +2,12 @@
 function withd
   if test -z $_WITHD_NEST
     pushd $argv[1]; or return 2
+    set -e argv[1]
     set -x _WITHD_NEST 1
-    fish -c $argv[2]
+
+    fish -c "$argv"
     set -l return_status $last_status
+
     popd
     set -e _WITHD_NEST
     return $return_status
