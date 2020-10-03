1341576de297d53abb79600701ed1262a693a0ec
Walter Derezinski <walter@daringway.com>
2020-10-03 17:09:53 -0400

    fixed installers for PATH

---
+++ by_shell/fish/functions/_shell_bin_dirs.fish
@@ -0,0 +1,8 @@
+
+function _shell_bin_dirs
+  for SHELL in bash zsh fish
+    if type $SHELL >/dev/null 2>&1
+      echo "$DARING_CLI_TOOLS_DIR/by_shell/$SHELL/bin"
+    end
+  end
+end
--- etc/rc.fish
+++ etc/rc.fish
@@ -2,11 +2,12 @@
 set DARING_CLI_TOOLS_DIR (dirname (dirname (realpath (status -f))))
 
 for FUNCTION_DIR in $DARING_CLI_TOOLS_DIR/by_shell/fish/functions
-  test -d $FUNCTION_DIR; and contains $FUNCTION_DIR $fish_function_path; or set -p fish_function_path $FUNCTION_DIR
+  test -d $FUNCTION_DIR; and ! contains $FUNCTION_DIR $fish_function_path; and set -p fish_function_path $FUNCTION_DIR
 end
 
-for BIN_DIR in $DARING_CLI_TOOLS_DIR/by_os/(uname -s)/bin $DARING_CLI_TOOLS_DIR/bin
-  test -d $BIN_DIR; and contains $BIN_DIR $PATH; or set -p PATH $BIN_DIR
+for BIN_DIR in $DARING_CLI_TOOLS_DIR/by_os/(uname -s)/bin (_shell_bin_dirs)
+  echo checking $BIN_DIR x
+  test -d $BIN_DIR; and ! contains $BIN_DIR $PATH; and set -p PATH $BIN_DIR; and echo set
 end
 
 withd $DARING_CLI_TOOLS_DIR git-abcheck
