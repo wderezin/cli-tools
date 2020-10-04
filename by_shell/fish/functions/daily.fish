fe6c283b210377ac755bc57d0812956ff68dc82b
Walter Derezinski <walter@daringway.com>
2020-10-03 23:09:43 -0400

    added daily fish function

---
+++ by_shell/fish/functions/daily.fish
@@ -0,0 +1,10 @@
+
+function daily --description "Only run the command once per 24 hours"
+  set -l DAY_IN_SECONDS 86400
+  set CHECK_VAR $argv[1]
+  set -e argv[1]
+  if ! set -q $CHECK_VAR ||  test (date +%s) -gt (math $$CHECK_VAR + $DAY_IN_SECONDS)
+    set -U $CHECK_VAR (date +%s)
+    eval $argv
+  end
+end
