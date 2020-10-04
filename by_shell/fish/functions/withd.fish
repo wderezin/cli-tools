6e53d864f422f3a7db883c16aaa140a8f3c9d893
Walter Derezinski <walter@daringway.com>
2020-10-03 22:42:16 -0400

    updated withd

--- by_shell/fish/functions/withd.fish
+++ by_shell/fish/functions/withd.fish
@@ -1,15 +1,11 @@
 
 function withd --description "Change to directory and run the command"
-  if test -z $_WITHD_NEST
-    pushd $argv[1]; or return 2
-    set -e argv[1]
-    set -x _WITHD_NEST 1
+  pushd $argv[1]; or return 2
+  set -e argv[1]
 
-    fish -c "$argv"
-    set -l return_status $last_status
+  eval $argv
+  set -l return_status $last_status
 
-    popd
-    set -e _WITHD_NEST
-    return $return_status
-  end
+  popd
+  return $return_status
 end
--- etc/rc.fish
+++ etc/rc.fish
@@ -1,4 +1,3 @@
-
 set DARING_CLI_TOOLS_DIR (dirname (dirname (realpath (status -f))))
 
 for FUNCTION_DIR in $DARING_CLI_TOOLS_DIR/by_shell/fish/functions
@@ -6,12 +5,11 @@ for FUNCTION_DIR in $DARING_CLI_TOOLS_DIR/by_shell/fish/functions
 end
 
 for BIN_DIR in $DARING_CLI_TOOLS_DIR/by_os/(uname -s)/bin (_shell_bin_dirs)
-  test -d $BIN_DIR; and ! contains $BIN_DIR $PATH; and set -p PATH $BIN_DIR; and echo set
+  test -d $BIN_DIR; and ! contains $BIN_DIR $PATH; and set -p PATH $BIN_DIR
 end
 
 set -l DAY_IN_SECONDS 86400
 if ! set -q DCT_LAST_CHECK ||  test (date +%s) -gt (math $DCT_LAST_CHECK + $DAY_IN_SECONDS)
   set -U DCT_LAST_CHECK (date +%s)
-  echo checking
   withd $DARING_CLI_TOOLS_DIR git-abcheck
 end
