d5850532e4546e6457a26057e6184bb31819dc27
Walter Derezinski <walter@daringway.com>
2020-11-17 21:13:07 -0500

    added PATH_brew for direnv

--- direnv.sh
+++ direnv.sh
@@ -3,3 +3,13 @@ nvm() {
     source $(brew --prefix nvm)/nvm.sh --no-use
     nvm $*
 }
+
+PATH_brew() {
+  DIR=$(brew --prefix $1@$2)
+  if [[ -d $DIR/bin ]]
+  then
+    PATH_add $DIR/bin
+  else
+    PATH_add $DIR
+  fi
+}
