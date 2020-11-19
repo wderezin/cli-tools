df278010c8501e8238d7bd82ea8d4c4c34f7d4ac
Walter Derezinski <walter@daringway.com>
2020-11-19 07:31:49 -0500

    added aws-ec2-ssh and refactored bin

--- etc/rc.fish
+++ etc/rc.fish
@@ -1,25 +1,17 @@
 set -g DARING_CLI_TOOLS_DIR (dirname (dirname (realpath (status -f))))
 
-for FUNCTION_DIR in $DARING_CLI_TOOLS_DIR/by_shell/fish/functions
-  test -d $FUNCTION_DIR; and ! contains $FUNCTION_DIR $fish_function_path; and set -p fish_function_path $FUNCTION_DIR
-end
+# Add function path
+set -l FUNCTION_DIR $DARING_CLI_TOOLS_DIR/by_shell/fish/functions
+test -d $FUNCTION_DIR; and ! contains $FUNCTION_DIR $fish_function_path; and set -p fish_function_path $FUNCTION_DIR
 
 # Make sure all event functions are loaded
 for func in (functions -a | grep '-event$')
   functions -D $func > /dev/null
 end
 
-# Add bin directories to PATH
-for DIR in $DARING_CLI_TOOLS_DIR/by_os/(uname -s)/bin
-  test -d $DIR; and ! contains $DIR PATH; and set -p PATH $DIR
-end
-
-for SHELL in (withd $DARING_CLI_TOOLS_DIR/by_shell command ls)
-  set -l DIR $DARING_CLI_TOOLS_DIR/by_shell/$SHELL/bin
-  if type $SHELL >/dev/null 2>&1; and test -d $DIR; and ! contains $DIR $PATH
-    set -p PATH $DIR
-  end
-end
+# Add bin bin to PATH
+set -l BIN_DIR $DARING_CLI_TOOLS_DIR/bin
+! contains $BIN_DIR  PATH; and set -p PATH $BIN_DIR
 
 if status --is-interactive
   daily-check DCT_LAST_CHECK "withd $DARING_CLI_TOOLS_DIR git-abcheck"
--- etc/rc.zsh
+++ etc/rc.zsh
@@ -2,20 +2,7 @@
 CURRENT=${0}
 export DARING_CLI_TOOLS_DIR=$(cd $(dirname $CURRENT)/..; pwd)
 
-function _shell_bin_dirs {
-  for SHELL in $(cd $DARING_CLI_TOOLS_DIR/by_shell; ls)
-  do
-    if type $SHELL >/dev/null 2>&1
-    then
-      echo "$DARING_CLI_TOOLS_DIR/by_shell/${SHELL}/bin "
-    fi
-  done
-}
-
-for BIN_DIR in $DARING_CLI_TOOLS_DIR/by_os/$(uname -s)/bin $(_shell_bin_dirs)
-do
-  test -d $BIN_DIR && [[ ":$PATH:" != *":$BIN_DIR:"* ]] && PATH=$BIN_DIR:$PATH
-done
+[[ ":$PATH:" != *":$DARING_CLI_TOOLS_DIR/bin:"* ]] && PATH=$DARING_CLI_TOOLS_DIR/bin:$PATH
 
 if [[ $- == *i* ]]
 then
