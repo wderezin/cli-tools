87215449031a85bbb5e863fa0a91ee1ec2cd62b9
Walter Derezinski <walter@daringway.com>
2020-11-30 11:50:13 -0500

    added prompt_git support for detached head

--- by_shell/fish/functions/prompt_git.fish
+++ by_shell/fish/functions/prompt_git.fish
@@ -19,6 +19,28 @@ function dot
   echo -n -s ' · '
 end
 
+function print_path 
+  if $dare_prompt_git_path
+    if test "$git_path_prefix" = ""
+      prompt_pwd
+    else 
+      echo -n 'git:'(basename $git_path_prefix)
+      # +2 to move just past /
+      set -l tmp (string sub --start=(math (string length $git_path_prefix) + 2) $PWD)
+
+      if test $fish_prompt_pwd_dir_length -eq 0
+          echo -n $blue/$tmp$normal
+      else
+          # Shorten to at most $fish_prompt_pwd_dir_length characters per directory
+          echo -n $blue(string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1/' /$tmp)$normal
+      end
+    end
+
+    dot
+  end
+end
+
+
 function prompt_git
   set -l cyan (set_color cyan)
   set -l yellow (set_color yellow)
@@ -29,40 +51,23 @@ function prompt_git
 
   # This allows overriding fish_prompt_pwd_dir_length from the outside (global or universal) without leaking it
   set -q fish_prompt_pwd_dir_length
-  or set -l fish_prompt_pwd_dir_length 1
+  or set -g fish_prompt_pwd_dir_length 1
 
   set -q dare_prompt_git_path
-  or set -l dare_prompt_git_path true
+  or set -g dare_prompt_git_path true
 
   set -q dare_prompt_git_remote
   or set -g dare_prompt_git_remote "origin"
 
   set -q dare_prompt_git_ahead_behind_count
-  or set -l dare_prompt_git_ahead_behind_count true
+  or set -g dare_prompt_git_ahead_behind_count true
 
   # Show git branch and status
   if test (_git_branch_name)
     set -l git_branch (_git_branch_name)
     set -l git_path_prefix (command git rev-parse --show-toplevel)
 
-    if $dare_prompt_git_path
-      if test "$git_path_prefix" = ""
-        prompt_pwd
-      else 
-        echo -n 'git:'(basename $git_path_prefix)
-        # +2 to move just past /
-        set -l tmp (string sub --start=(math (string length $git_path_prefix) + 2) $PWD)
-
-        if test $fish_prompt_pwd_dir_length -eq 0
-            echo -n $blue/$tmp$normal
-        else
-            # Shorten to at most $fish_prompt_pwd_dir_length characters per directory
-            echo -n $blue(string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1/' /$tmp)$normal
-        end
-      end
-
-      dot
-    end
+    print_path
 
     if set ab (_git_ahead_behind_count $git_branch)
       if test $ab[1] -gt 0; or test $ab[2] -gt 0
@@ -96,6 +101,10 @@ function prompt_git
     end
     echo -n -s $git_info $normal
 
+    return 0
+  else if git branch -l HEAD | grep 'detached' >/dev/null 2>/dev/null
+    print_path
+    echo -n -s $red '(detached)' $normal
     return 0
   end
 
