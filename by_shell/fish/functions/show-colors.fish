c79af3871c30c06d07350af88832e998cdeb1219
Walter Derezinski <walter@daringway.com>
2020-12-08 11:13:19 -0500

    added direnv use_aws_credentials

--- by_shell/fish/functions/show-colors.fish
+++ by_shell/fish/functions/show-colors.fish
@@ -1,5 +1,5 @@
 function show-colors
-    set -n  | egrep '^fish_color' | while read x
+    set -n  | egrep '^fish' | grep color | while read x
         echo  (set_color $$x)$x(set_color $fish_color_normal)
     end
 end
--- direnv.sh
+++ direnv.sh
@@ -33,7 +33,7 @@ use_aws_sso() {
       eval "$(aws2-wrap --profile ${AWS_PROFILE} --export)"
       if [ "${AWS_SESSION_TOKEN}" != "" ]
       then
-        # Used by fish shell _aws-credential-event function
+        # Used by fish shell _aws-credential-event prompt_account functions
         export AWS_AUTH_ON="$(date)"
       else
         export AWS_AUTH_ON=""
@@ -41,4 +41,27 @@ use_aws_sso() {
   fi
 
   watch_file  ~/.aws/sso/cache/*.json
+}
+
+use_aws_credentials() {
+  AWS_REGION=${AWS_REGION-us-east-1}
+
+  if test -n "$1"
+  then
+    export AWS_PROFILE=$1
+  fi 
+
+  if [ -n "${AWS_PROFILE}" ]
+  then
+      if command aws sts get-caller-identity >/dev/null 2>&1
+      then
+        # Used by fish shell _aws-credential-event and prompt_account functions
+        export AWS_AUTH_ON="$(date)"
+      else
+        export AWS_AUTH_ON=""
+      fi
+  fi
+
+
+  watch_file ~/.aws/credentials
 }
