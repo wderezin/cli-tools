1a71b7c0653848b332f136ab5e3289c6cefa07df
Walter Derezinski <walter@daringway.com>
2021-01-28 07:15:09 -0500

    bug in  _direnv-reload-on-pwd-event

--- by_shell/fish/functions/_direnv-reload-on-pwd-event.fish
+++ by_shell/fish/functions/_direnv-reload-on-pwd-event.fish
@@ -3,7 +3,7 @@
 function _direnv-reload-on-pwd-event --on-event fish_prompt
     if test "$PREVIOUS_PWD" != "$PWD"
         if is-enabled $DIRENV_RELOAD_ON_PWD; and direnv status | grep 'Found RC allowed true' >/dev/null
-            set -g -x CURRENT_PWD $PWD
+            set -g -x PREVIOUS_PWD $PWD
             direnv reload
         end
     end
