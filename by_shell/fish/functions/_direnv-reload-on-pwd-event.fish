5993846e6ff59e0811357c35c458e476cd1d8cf7
Walter Derezinski <walter@daringway.com>
2021-01-28 01:04:10 -0500

    updated direnv-reload-on-pwd fish event to only run on prompt and if PWD changed

--- by_shell/fish/functions/_direnv-reload-on-pwd-event.fish
+++ by_shell/fish/functions/_direnv-reload-on-pwd-event.fish
@@ -1,8 +1,10 @@
 
 # Used in combination with direnv reload_on_pwd
-function _direnv-reload-on-pwd-event --on-variable="PWD"
-    if is-enabled $DIRENV_RELOAD_ON_PWD; and direnv status | grep 'Found RC allowed true' >/dev/null
-        set -g -x CURRENT_PWD $PWD
-        direnv reload
+function _direnv-reload-on-pwd-event --on-event fish_prompt
+    if test "$PREVIOUS_PWD" != "$PWD"
+        if is-enabled $DIRENV_RELOAD_ON_PWD; and direnv status | grep 'Found RC allowed true' >/dev/null
+            set -g -x CURRENT_PWD $PWD
+            direnv reload
+        end
     end
 end
