141b56a1795839d8592455b4c5ef5ae95b697f98
Walter Derezinski <walter@daringway.com>
2022-06-09 23:20:45 -0400

    Update date-to-epoch to support Z timezone on bsd

--- by_shell/fish/functions/date-to-epoch.fish
+++ by_shell/fish/functions/date-to-epoch.fish
@@ -2,7 +2,7 @@
 function date-to-epoch -a date_string
 
     if-linux echo (date "+%s" -d (echo $date_string | perl -pe 's/:(\d\d)$/\1/; s/T/ /') 2>/dev/null)
-    or if-bsd echo (date -j -f "%Y-%m-%d %H:%M:%S%z" (echo $date_string | perl -pe 's/:(\d\d)$/\1/; s/T/ /') +%s 2>/dev/null)
+    or if-bsd echo (date -j -f "%Y-%m-%d %H:%M:%S%Z" (echo $date_string | perl -pe 's/:(\d\d)$/\1/; s/T/ /; s/Z/GMT/') +%s 2>/dev/null)
     or if-bsd echo (date -j -f "%s" $date_string +%s 2>/dev/null)
     or echo 0
 
