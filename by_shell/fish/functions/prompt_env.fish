9e4251d77e5ecf89b6502297f1a9f22f489430d6
Walter Derezinski <walter@daringway.com>
2022-06-14 18:08:13 -0400

    Fixed AMPLIFY_ENV unset

--- by_shell/fish/functions/prompt_env.fish
+++ by_shell/fish/functions/prompt_env.fish
@@ -5,6 +5,8 @@ function prompt_env
     if test -f $ROOTDIR/amplify/.config/local-env-info.json
         set -a env_info (jq -r '.envName' $ROOTDIR/amplify/.config/local-env-info.json)
         set -g AMPLIFY_ENV $env_info
+    else
+        set -e AMPLIFY_ENV
     end
 
     if set -q env_info
